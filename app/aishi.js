8530d337efab44bc689127fc4567364ec159c192
Aishi Jiang <jiangaishi@gmail.com>
2017-01-29 06:53:34 -0500

    heroku push

--- app/aishi.js
+++ app/aishi.js
@@ -1,7 +1,6 @@
 
 
 
-
 // passes an array of notes:
 //Note: 1 --> clock: 100, MIDI channel: 0, note: F5, velocity: 127, duration: 100 clocks
 //Note: 2 --> clock: 200, MIDI channel: 0, note: E5, velocity: 127, duration: 50 clocks
@@ -197,4 +196,51 @@ function playback(num){
   });
 }
 */
+
+
+  function updateTimeSpan() {
+    var theSpan = document.getElementById('timeLeft');
+    var d = new Date();
+    var t = new Date();
+    var ms;
+    var s, m, h;
+
+    // if it's after 5pm, set today to tomorrow
+    if (d.getHours() > 16){ d.setDate(d.getDate() + 1); }
+    // get the next monday
+    while (d.getDay() != 1){ d.setDate(d.getDate() + 1); }
+    // set the time
+    d.setHours(23);
+    d.setMinutes(18);
+    d.setSeconds(0);
+    // get the difference between right now and next monday
+    ms = d - t;
+    // get the days between now and then
+    d = parseInt(ms / (1000 * 60 * 60 * 24));
+    ms -= (d * 1000 * 60 * 60 * 24);
+    // get hours
+    h = parseInt(ms / (1000 * 60 * 60));
+    ms -= (h * 1000 * 60 * 60);
+    // get minutes
+    m = parseInt(ms / (1000 * 60));
+    ms -= (m * 1000 * 60);
+    // get seconds
+    s = parseInt(ms / 1000);
+    theSpan.innerHTML = d + ' days, ' + h + ' hours, ' + m + ' minutes, and ' + s + ' seconds.';
+    setTimeout('updateTimeSpan()', 100);
+}
+
+onload = updateTimeSpan();
+
+$(function() {
+
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
+
 
